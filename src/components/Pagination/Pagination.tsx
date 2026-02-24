056ae5b5f054172207504afa69e2b810f65432ce
Akimov Max <maxymus2828@gmail.com>
2026-02-24 18:24:58 +0200

    pre setup

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,19 @@
+import ReactPaginate from "react-paginate";
+
+import css from "./Pagination.module.css";
+
+export default function Pagination({ totalPages, currentPage, onPageChange }) {
+  return (
+    <ReactPaginate
+      pageCount={totalPages}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }) => onPageChange(selected + 1)}
+      forcePage={currentPage - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
+  );
+}
